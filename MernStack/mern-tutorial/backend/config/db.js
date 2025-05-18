1039480960b80ed8d8a820496e7f324f7d8b684a
M Malysa <mariastud2023@gmail.com>
2025-05-19 00:51:25 +0300

    MernStack_playlist

--- MernStack/mern-tutorial/backend/config/db.js
+++ MernStack/mern-tutorial/backend/config/db.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect(
+      'mongodb+srv://admin:admin@cluster0.spcnpip.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: "mernapp" // ← tutaj podaj nazwę swojej bazy danych
+      }
+    );
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(error)
-    process.exit(1)
+    console.log(error);
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
